6b9577d77fffdbf89009576e369d93b6480b8504
Lin Shi <shilin@dyn-129-236-234-76.dyn.columbia.edu>
2017-03-22 23:25:48 -0400

    changed profile page

--- skeleton/server.py
+++ skeleton/server.py
@@ -22,36 +22,9 @@ from flask import Flask, request, render_template, g, redirect, Response
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
 
-
-#
-# The following is a dummy URI that does not connect to a valid database. You will need to modify it to connect to your Part 2 database in order to use the data.
-#
-# XXX: The URI should be in the format of: 
-#
-#     postgresql://USER:PASSWORD@104.196.18.7/w4111
-#
-# For example, if you had username biliris and password foobar, then the following line would be:
-#
-#     DATABASEURI = "postgresql://biliris:foobar@104.196.18.7/w4111"
-#
 DATABASEURI = "postgresql://yd2369:Keineanung20@104.196.18.7/w4111"
 
-
-#
-# This line creates a database engine that knows how to connect to the URI above.
-#
 engine = create_engine(DATABASEURI)
-
-#
-# Example of running queries in your database
-# Note that this will probably not work if you already have a table named 'test' in your database, containing meaningful data. This is only an example showing you how to run queries in your database using SQLAlchemy.
-#
-engine.execute("""CREATE TABLE IF NOT EXISTS test (
-  id serial,
-  name text
-);""")
-engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
-
 
 @app.before_request
 def before_request():
@@ -109,69 +82,26 @@ def index():
   # DEBUG: this is debugging code to see what request looks like
   print request.args
 
-
-  #
-  # example of a database query
-  #
   cursor = g.conn.execute("SELECT name FROM test")
   names = []
   for result in cursor:
-    names.append(result['name'])  # can also be accessed using result[0]
+    names.append(result['name'])  
   cursor.close()
-  
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #     
-  #     # creates a <div> tag for each element in data
-  #     # will print: 
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
   context = dict(data = names)
-
-
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
   return render_template("index.html", **context)
 
 @app.route('/profile')
 def profile():
-  cursor = g.conn.execute("SELECT * FROM users U WHERE U.uid = ?", uname)
+  cursor = g.conn.execute("SELECT * FROM users U WHERE U.uid = '10002'")
   information = []
   for result in cursor:
-    information.append(result)  # can also be accessed using result[0]
+      for column in result:
+          information.append(result)
   cursor.close()
   context = dict(data = information)
-  return render_template("profile.html", **context)
+  return render_template("another.html", **context)
 
-#
-# This is an example of a different path.  You can see it at:
-# 
-#     localhost:8111/another
-#
-# Notice that the function name is another() rather than index()
-# The functions for each app.route need to have different names
-#
+
 @app.route('/another')
 def another():
   cursor = g.conn.execute("SELECT * FROM users U WHERE U.uid='10001' ")
@@ -197,6 +127,7 @@ def login():
     name = []
     name.append(request.form['name'])
     uname = dict(data = name)
+    print(uname)
     return render_template("profile.html", **uname)
 
 
