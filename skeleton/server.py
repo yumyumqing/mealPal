ab0f7c7a9951802c6d4a99f297dedcacfcbf73cd
Yanqing Dai <yd2369@columbia.edu>
2017-04-06 14:50:54 -0400

    port and ip changed back

--- skeleton/server.py
+++ skeleton/server.py
@@ -527,8 +527,8 @@ if __name__ == "__main__":
   @click.command()
   @click.option('--debug', is_flag=True)
   @click.option('--threaded', is_flag=True)
-  @click.argument('HOST', default='35.185.238.36')
-  @click.argument('PORT', default=8000, type=int)
+  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('PORT', default=8111, type=int)
   def run(debug, threaded, host, port):
     """
     This function handles command line parameters.
